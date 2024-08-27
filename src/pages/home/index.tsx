003948e8c3ec002c16f77076f793bc9dfccb232a
Pradip bhusnar <bhusnarpradip@gmail.com>
2024-08-27 11:30:43 +0530

    PB | updated dashboard

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,30 +5,18 @@ const Home = () => {
         <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-              Building digital <br />
-              products & brands.
+              Streamline Your Sea Shipments <br />
+              with Confidence.
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              This free and open-source landing page template was built using the utility classes from{' '}
-              <a href="https://tailwindcss.com" className="hover:underline">
-                Tailwind CSS
-              </a>{' '}
-              and based on the components from the{' '}
-              <a href="https://flowbite.com/docs/getting-started/introduction/" className="hover:underline">
-                Flowbite Library
-              </a>{' '}
-              and the{' '}
-              <a href="https://flowbite.com/blocks/" className="hover:underline">
-                Blocks System
-              </a>
-              .
+              Effortlessly manage, track, and deliver shipments on time with our all-in-one sea shipment management platform.
             </p>
             <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
               <a
-                href="https://github.com/themesberg/landwind"
+                href="/register"
                 className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
               >
-                <svg className="w-4 h-4 mr-2 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" />
+                Get Started Now
               </a>
               <a
                 href="https://www.figma.com/community/file/1125744163617429490"
@@ -43,12 +31,12 @@ const Home = () => {
                   <path id="path2_fill" className="st3" d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50V0z" />
                   <path id="path3_fill" className="st4" d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z" />
                 </svg>{' '}
-                Get Figma file
+                Explore Features
               </a>
             </div>
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="./assets/images/hero.png" alt="hero image" />
+            <img src="./assets/images/hero1.webp" alt="hero image" />
           </div>
         </div>
       </section>
@@ -258,62 +246,10 @@ const Home = () => {
         <div className="max-w-screen-xl px-4 py-8 mx-auto space-y-12 lg:space-y-20 lg:py-24 lg:px-6">
           <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
             <div className="text-gray-500 sm:text-lg dark:text-gray-400">
-              <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Work with tools you already use</h2>
+              <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Real-Time Shipment Tracking</h2>
               <p className="mb-8 font-light lg:text-xl">
-                Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work,
-                eliminate toil, and deploy changes with ease.
+                Monitor your shipments' journey from origin to destination with our real-time tracking feature, ensuring you’re always in the loop.
               </p>
-              {/* <!-- List --> */}
-              <ul role="list" className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700">
-                <li className="flex space-x-3">
-                  {/* <!-- Icon --> */}
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Continuous integration and deployment</span>
-                </li>
-                <li className="flex space-x-3">
-                  {/* <!-- Icon --> */}
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Development workflow</span>
-                </li>
-                <li className="flex space-x-3">
-                  {/* <!-- Icon --> */}
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Knowledge management</span>
-                </li>
-              </ul>
               <p className="mb-8 font-light lg:text-xl">
                 Deliver great service experiences fast - without the complexity of traditional ITSM solutions.
               </p>
@@ -324,95 +260,32 @@ const Home = () => {
           <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
             <img className="hidden w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="./assets/images/feature-2.png" alt="feature image 2" />
             <div className="text-gray-500 sm:text-lg dark:text-gray-400">
-              <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">We invest in the world’s potential</h2>
+              <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Comprehensive Dashboard</h2>
               <p className="mb-8 font-light lg:text-xl">
-                Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work,
-                eliminate toil, and deploy changes with ease.
+                Access all the crucial details of your shipments in one place. From shipment status to estimated delivery dates, our dashboard
+                provides a complete overview.
               </p>
-              {/* <!-- List --> */}
-              <ul role="list" className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700">
-                <li className="flex space-x-3">
-                  {/* <!-- Icon --> */}
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Dynamic reports and dashboards</span>
-                </li>
-                <li className="flex space-x-3">
-                  {/* <!-- Icon --> */}
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Templates for everyone</span>
-                </li>
-                <li className="flex space-x-3">
-                  {/* <!-- Icon --> */}
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Development workflow</span>
-                </li>
-                <li className="flex space-x-3">
-                  {/* <!-- Icon --> */}
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Limitless business automation</span>
-                </li>
-                <li className="flex space-x-3">
-                  {/* <!-- Icon --> */}
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Knowledge management</span>
-                </li>
-              </ul>
               <p className="font-light lg:text-xl">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.</p>
+            </div>
+          </div>
+
+          <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
+            <div className="text-gray-500 sm:text-lg dark:text-gray-400">
+              <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Efficient Customer Onboarding</h2>
+              <p className="mb-8 font-light lg:text-xl">
+                Easily onboard customers and manage their shipment records, making your operations smoother and more efficient.
+              </p>
+            </div>
+            <img className="hidden w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="./assets/images/feature-1.png" alt="dashboard feature image" />
+          </div>
+
+          <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
+            <img className="hidden w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="./assets/images/feature-2.png" alt="feature image 2" />
+            <div className="text-gray-500 sm:text-lg dark:text-gray-400">
+              <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white"> Automated Notifications</h2>
+              <p className="mb-8 font-light lg:text-xl">
+                Keep your customers informed at every step with automated email notifications for shipment updates, delays, and deliveries.
+              </p>
             </div>
           </div>
         </div>
